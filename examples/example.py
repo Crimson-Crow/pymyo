91766e14ba3777e6571e266f6ba15b18ce96a00d
Crimson-Crow <github@crimsoncrow.dev>
2021-06-28 20:34:44 -0400

    Modify basic example for simplicity

--- examples/example.py
+++ examples/example.py
@@ -8,8 +8,6 @@ MYO_ADDRESS = 'CC:B3:25:0D:5B:C3'
 async def main():
     async with Myo(MYO_ADDRESS) as myo:
         print('Device name:', await myo.name)
-        # myo.name = 'test'
-        # print('Device name:', myo.name)
         print('Battery level:', await myo.battery)
         print('Serial number:', myo.serial_number)
         print('Firmware version:', myo.firmware_version)
@@ -22,19 +20,9 @@ async def main():
 
         await myo.vibrate2(((250, 255), (250, 128), (250, 255), (0, 0), (0, 0), (0, 0)))
 
-        from collections import deque
-        time_log = deque([], 100)
-
-        last_call_time = time.time()
-
         @myo.bind(Myo.Event.EMG)
         def on_emg(emg):
-            nonlocal last_call_time
-            new_call_time = time.time()
-            time_diff = new_call_time - last_call_time
-            time_log.append(time_diff)
-            print(sum(time_log)/len(time_log))
-            last_call_time = new_call_time
+            print(emg)
 
         await myo.set_sleep_mode(SleepMode.NEVER_SLEEP)
         await myo.set_mode(emg_mode=EmgMode.EMG)
